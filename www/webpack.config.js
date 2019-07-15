a9de5ed0573e859cf84cab00d479d993e1a4cb8d
Joshua Byrd <phocks@gmail.com>
2019-07-15 16:47:19 +1000

    Hello

---
+++ www/webpack.config.js
@@ -0,0 +1,28 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin(['index.html'])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        // exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
